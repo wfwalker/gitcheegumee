6247b68eb8b011487df25fc1407d3a9916ec66a5
Bill Walker <bwalker@mozilla.com>
2011-10-05 12:09:17 -0700

    add session passphrase (i bet this should not be on github)

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,8 @@ Rails::Initializer.run do |config|
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
 
+  config.action_controller.session = { :key => "_myapp_session", :secret => "some other really secret phrase of at least 30 characters" }
+
   # Add additional load paths for your own custom dirs
   # config.autoload_paths += %W( #{RAILS_ROOT}/extras )
 
