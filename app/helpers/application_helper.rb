ebeae9371747f6a105269936b31dbac8b9f8959e
Bill Walker <bwalker@mozilla.com>
2011-10-19 08:46:12 -0700

    trial use of gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
+require 'digest/md5'
+
 module ApplicationHelper
+	def gravatar_url_for(email, options = {})
+		url_for({
+			:gravatar_id => Digest::MD5.hexdigest(email), 
+			:host => 'www.gravatar.com', :protocol => 'http://', 
+			:only_path => false, 
+			:controller => 'avatar.php'
+		}.merge(options))
+	end
 end
