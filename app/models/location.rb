9f8a9e2a653da3a3346e7237b2234675f283c68e
Bill Walker <bwalker@mozilla.com>
2012-01-10 12:26:29 -0800

    define entrances for a location, show in the listing if a location has no entrances (parallels what we do with exits)

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
 	has_many :exits, :class_name => 'Passage', :foreign_key => 'source_id'
+	has_many :entrances, :class_name => 'Passage', :foreign_key => 'destination_id'
 	has_many :players
 	has_many :items
 	has_many :happenings do
