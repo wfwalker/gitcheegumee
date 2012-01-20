c1f8697aeb39d8417d5e59622128f449c7ea692c
Bill Walker <bwalker@mozilla.com>
2012-01-19 22:52:45 -0800

    add method to compute recently modified players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ActiveRecord::Base
+	def Player.recently_updated
+		Player.find(:all, :conditions => ['updated_at >= ?', Time.now - 30.minute])
+	end
+
 	belongs_to :location
 	has_many :items
 	has_many :happenings do
@@ -6,7 +10,7 @@ class Player < ActiveRecord::Base
 			Happening.latest(self)
 		end
 	end
-
+  	
 	validates_presence_of :name, :email, :location_id
 	validates_uniqueness_of :name, :email
 	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"  	
