9ede7d8aed8ee8843ffafd3af696dfcbb30aa476
Bill Walker <bwalker@mozilla.com>
2012-01-22 23:36:32 -0800

    don't show locations unless you've got admin privs. ordinary players don't need show action for location, they need play action for players...

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,7 @@
 class LocationsController < ApplicationController
 
-  before_filter :verify_admin_credentials, :only => [:new, :create, :edit, :index, :update, :destroy]  
-  before_filter :verify_and_update_activity_timer, :except => [:new, :create, :edit, :index, :update, :destroy]  
+  before_filter :verify_admin_credentials, :only => [:new, :show, :create, :edit, :index, :update, :destroy]  
+  before_filter :verify_and_update_activity_timer, :except => [:new, :show, :create, :edit, :index, :update, :destroy]  
 
   # GET /locations
   # GET /locations.xml
